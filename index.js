4e7c3254718aea04f5a929c4777bc8d1071ea0cd
Elmer Villegas <elmer.villegas@utec.edu.pe>
2025-08-29 17:02:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { db, init } = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 8000;
 
 app.use(cors());
 app.use(express.json());
